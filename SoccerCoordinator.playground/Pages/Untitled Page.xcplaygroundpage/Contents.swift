31865690d227f809a3683a74bef7c8ae9c15440e
Kruzpe <pepe.cruz19@gmail.com>
2017-07-28 16:26:38 -0500

    collection finished

---
+++ SoccerCoordinator.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -0,0 +1,24 @@
+var players = [
+    
+    "Joe Smith": ["height": 42,"soccer experience": "yes","guardians name´s": "Jim and Jan Smith"],
+    "Jill Tanner": ["height": 36,"soccer experience": "yes","guardians name´s": "Clara Tanner"],
+    "Bill Bon": ["height": 43,"soccer experience": "yes","guardians name´s": "Sara and Jenny Bon"],
+    "Eva Gordon": ["height": 45,"soccer experience": "no","guardians name´s": "Wendy and Mike Gordon"],
+    "Matt Gill": ["height": 40,"soccer experience": "no","guardians name´s": "Charles and Sylvia Gill"],
+    "Kimmy Stein": ["height": 41,"soccer experience": "no","guardians name´s": "Bill and Gillary Stein"],
+    "Sammy Adams": ["height": 45,"soccer experience": "no","guardians name´s": "Jeff Adams"],
+    "Karl Saygan": ["height": 42,"soccer experience": "yes","guardians name´s": "Heather Bledsoe"],
+    "Suzane Greenberg": ["height": 44,"soccer experience": "yes","guardians name´s": "Henrietta Dumas"],
+    "Sal Dali": ["height": 41,"soccer experience": "no","guardians name´s": "Gala Dali"],
+    "Joe kavalier": ["height": 39,"soccer experience": "no","guardians name´s": "Sam and Elaine Kavalier"],
+    "Ben Finkelstein": ["height": 44,"soccer experience": "no","guardians name´s": "Aaron and Jill Finkelstein"],
+    "Diego Soto": ["height": 41,"soccer experience": "yes","guardians name´s": "Robin and Sarika Soto"],
+    "Chloe Alaska": ["height": 47,"soccer experience": "no","guardians name´s": "David and Jamie Alaska"],
+    "Arnold Willis": ["height": 43,"soccer experience": "no","guardians name´s": "Claire Willis"],
+    "Philip Helm": ["height": 44,"soccer experience": "yes","guardians name´s": "Thomas Helm and Eva Jones"],
+    "Les Clay": ["height": 42,"soccer experience": "yes","guardians name´s": "Wynonna Brown"],
+    "Herschel Krustofski": ["height": 45,"soccer experience": "yes","guardians name´s": "Hyman and Rachel Krustofski"]
+    ] as [String : Any]
+
+
+
