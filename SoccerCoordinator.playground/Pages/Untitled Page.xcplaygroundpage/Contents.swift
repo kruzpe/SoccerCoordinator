5d1597f56d0e21a20b79b5f575de46ca82b0fb41
Kruzpe <pepe.cruz19@gmail.com>
2017-08-16 18:13:41 -0500

    final version

--- SoccerCoordinator.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
+++ SoccerCoordinator.playground/Pages/Untitled Page.xcplaygroundpage/Contents.swift
@@ -20,12 +20,13 @@ let players: [[String: Any]] = [
     ["name": "Les Clay" ,"height": 42,"soccer experience": "yes","guardians name´s": "Wynonna Brown"],
     ["name": "Herschel Krustofski" ,"height": 45,"soccer experience": "yes","guardians name´s": "Hyman and Rachel Krustofski"]
 ]
-
-func order(team: [[String: Any]]) -> ([[String: Any]],[[String: Any]],[[String: Any]]) {
+// Function that receives the array of players and returns three arrays for each team
+func bestTeam(team: [[String: Any]]) -> (teamSharks: [[String: Any]],teamDragons: [[String: Any]],teamRaptors: [[String: Any]]) {
     
     var orderByHeight = [[String:Any]]()
     
-    //************************************************************************************
+//************************************************************************************
+//To obtain the average we separate the boys who have experience and those who do not and order them in a descending and ascending way respectively
     var ascendingOrder = [[String:Any]]()
     
     for soccerPlayer in team {
@@ -59,7 +60,7 @@ func order(team: [[String: Any]]) -> ([[String: Any]],[[String: Any]],[[String: 
             
         }
     }
-    // ***************************************************************************************
+// ***************************************************************************************
     var descendingOrder = [[String: Any]]()
     for soccerPlayer in players {
         let soccerExperience = String(describing: soccerPlayer["soccer experience"]!)
@@ -90,22 +91,95 @@ func order(team: [[String: Any]]) -> ([[String: Any]],[[String: Any]],[[String: 
             
         }
     }
-    // ************************************************************************************
-    
-    
+    // We join the result of the two arrays in such a way that the distribution of height us of a balanced average
     orderByHeight = descendingOrder + ascendingOrder
     
     var teamSharks = [[String : Any]]()
     var teamDragons = [[String : Any]]()
     var teamRaptors = [[String : Any]]()
     
+    
+    var positionRight = 0
+    
+    for  var player in orderByHeight {
+        switch positionRight {
+        case 0:
+            player.updateValue("Sharks", forKey: "team")
+            teamSharks.append(player)
+            positionRight = 1
+        case 1:
+            player.updateValue("Dragons", forKey: "team")
+            teamDragons.append(player)
+            positionRight = 2
+        default:
+            player.updateValue("Raptors", forKey: "team")
+            teamRaptors.append(player)
+            positionRight = 0
+        
+        }
+        
+    }
 
     
     return (teamSharks,teamDragons,teamRaptors)
 }
 
+var team = bestTeam(team: players)
+var teamSharks = team.teamSharks
+var teamDragons = team.teamDragons
+var teamRaptors = team.teamRaptors
+
+var letters = [String]()
 
 
+//Function that receives the array of players and creates a letter depending on the equipment to which it belongs
+func letter(teams: [[String: Any]]){
+    
+    for player in teams {
+        let team = String(describing: player["team"]!)
+        switch team {
+        case "Sharks":
+            letters.append("Dear \(player["guardians name´s"]!) we inform you that the child \(player["name"]!) will be part of the \(player["team"]!) team and will be due on March 17th at 3 pm.")
+            case "Dragons":
+                letters.append("Dear \(player["guardians name´s"]!) we inform you that the child \(player["name"]!) will be part of the \(player["team"]!) team and will be due on March 17th at 1 pm.")
+        default:
+            letters.append("Dear \(player["guardians name´s"]!) we inform you that the child \(player["name"]!) will be part of the \(player["team"]!) team and will be due on March 18th at 1 pm.")
+        }
+        
+    }
+}
+
+//Function that gives us the average
+
+func average(team: [[String: Any]]) -> Float{
+    
+var average:Float = 0
+var totalHeight = 0
+let totalPlayer = Float(team.count)
+
+for player in teamSharks {
+    let height = player["height"] as! Int
+    totalHeight += height
+}
+    average = Float(totalHeight) / totalPlayer
+    return average
+}
+
+
+print("Averge of team Sharks is: \(average(team: teamSharks))")
+print("Averge of team Dragons is: \(average(team: teamDragons))")
+print("Averge of team Raptors is: \(average(team: teamRaptors))")
+// ********************************************************************************
+
+
+
+letter(teams: teamSharks)
+letter(teams: teamDragons)
+letter(teams: teamRaptors)
+
+for letter in letters {
+    print(letter)
+}
 
 
 
